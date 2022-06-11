fa23fec1cb6e6b0dd8d6029b9c9bfaa0d36348dd
benyapha <esrunursee@gmail.com>
2022-06-12 03:24:12 +0700

    add: header

---
+++ profile/src/Pages/HomePage.js
@@ -0,0 +1,14 @@
+import "../App.css";
+// import Nav from "../components/Nav";
+import Header from "../components/Header";
+
+function HomePage() {
+  return (
+    <div className="HomePage">
+      {/* <Nav /> */}
+      <Header />
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ profile/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      Blue800: "#1C1C65",
+      Blue900: "#0F103F",
+      white: "#ffffff",
+      black: "#000000",
+      Purple: "#A19DCA",
+      transparent: "transparent"
+    },
+    extend: {},
+  },
+  plugins: [],
+};
