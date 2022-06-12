e34d306451a4fd6c6ebb518d351ad30aedf653ac
benyapha <esrunursee@gmail.com>
2022-06-12 11:56:34 +0700

    header: add responsive

--- profile/src/components/Header.js
+++ profile/src/components/Header.js
@@ -3,26 +3,26 @@ import Frame from "../Assets/Frame.svg";
 
 function Header() {
   return (
-    <header className=" bg-Blue900 min-h-screen flex items-center">
-      <div className="ml-40 h-full flex pr-20">
-        <div className="flex flex-col justify-around h-[50vh] w-full relative top-[92px] left-32">
-          <h1 className="text-white text-[8vh] font-bold z-50">
+    <header className="bg-Blue900 min-h-screen flex items-center min-w-screen sm:justify-center">
+      <div className="lg:ml-40 h-full lg:flex lg:pr-20 sm:flex sm:flex-col-reverse sm:m-0 sm:h-screen sm:items-center">
+        <div className="flex flex-col justify-center lg:h-screen sm:h-[50vh] w-full relative lg:top-[-10px] lg:left-[10vw] sm:items-center">
+          <h1 className="text-white xl:text-5xl lg:text-4xl font-bold z-50 xl:leading-[80px] lg:leading-[60px] sm:text-5xl sm:leading-[80px]">
             I'm{" "}
-            <span dataText="Benyapha" id="name">
+            <span dataText="Benyapha" id="name" className="xl:text-5xl lg:text-4xl text-transparent xl:leading-[80px] lg:leading-[60px] sm:text-5xl sm:leading-[80px] font-bold relative z-50 before:absolute before:xl:leading-[80px] before:lg:leading-[60px] xl:top-0">
               Benyapha{" "}
             </span>
           </h1>
-          <h1 className="text-white text-[8vh] font-bold z-50">
+          <h1 className="text-white xl:text-5xl xl:leading-[80px] lg:text-4xl lg:leading-[60px] sm:text-5xl sm:leading-[80px] font-bold z-50">
             Front-end Developer
           </h1>
-          <h1 className="text-white text-[8vh] font-bold z-50">
+          <h1 className="text-white xl:text-5xl xl:leading-[80px] lg:text-4xl lg:leading-[60px] sm:text-5xl sm:leading-[80px] font-bold z-50">
             Based in Bangkok
           </h1>
-          <button className="bg-Purple w-fit mt-12 text-[3vh] font-normal text-white px-10 py-2 uppercase shadow-lg rounded-lg transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:brightness-110 duration-300">
+          <button className="bg-Purple w-fit lg:mt-12 sm:my-16 xl:text-xl font-medium lg:text-lg sm:text-base text-white px-10 py-2 uppercase shadow-lg rounded-lg transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:brightness-110 duration-300">
             hire me
           </button>
         </div>
-        <img className="w-[37vw]" src={Frame} alt="Women" />
+        <img className="w-[37vw] sm:w-[45vw]" src={Frame} alt="Women" />
       </div>
     </header>
   );
