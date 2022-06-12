3990d1e78efe2c95b05ff8cc8e7ced73eff3e6c5
benyapha <esrunursee@gmail.com>
2022-06-12 11:56:34 +0700

    header: add responsive

--- profile/tailwind.config.js
+++ profile/tailwind.config.js
@@ -10,6 +10,13 @@ module.exports = {
       Purple: "#A19DCA",
       transparent: "transparent"
     },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1600px'
+    },
     extend: {},
   },
   plugins: [],
